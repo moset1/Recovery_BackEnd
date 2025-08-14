3a4afe6a38ff4662a2b3b0964dfe41fb3ec47495
MoseT1 <kimmose981214@gmail.com>
2025-08-14 18:08:55 +0900

    fix : Optional을 사용하도록 추가

--- src/main/java/com/solution/demo/repository/ExerciseRepository.java
+++ src/main/java/com/solution/demo/repository/ExerciseRepository.java
@@ -3,7 +3,9 @@ package com.solution.demo.repository;
 import com.solution.demo.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+import java.util.Optional;
 
-    Exercise findByExerciseName(String exerciseName);
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+    // 운동 이름으로 운동 정보를 조회하고, 결과가 없을 수도 있음을 Optional로 명시
+    Optional<Exercise> findByExerciseName(String exerciseName);
 }
