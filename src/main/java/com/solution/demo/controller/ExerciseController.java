af3e8e0f75306dcfb75e4398a8fb72e2a309fd68
MoseT1 <kimmose981214@gmail.com>
2025-08-14 17:34:44 +0900

    fix : 엔드포인트를 @PathVariable로 리팩토링

--- src/main/java/com/solution/demo/controller/ExerciseController.java
+++ src/main/java/com/solution/demo/controller/ExerciseController.java
@@ -3,23 +3,23 @@ package com.solution.demo.controller;
 import com.solution.demo.entity.Exercise;
 import com.solution.demo.service.ExerciseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/exercise")
+@RequestMapping("/api/exercises")
 public class ExerciseController {
 
     private final ExerciseService exerciseService;
 
-    @GetMapping("/detail")
-    public ResponseEntity<Exercise> getExercise(@RequestParam String exerciseName) {
+    @GetMapping("/{exerciseName}")
+    public ResponseEntity<Exercise> getExercise(@PathVariable String exerciseName) {
         Exercise exercise = exerciseService.findByExerciseName(exerciseName);
-        return new ResponseEntity<>(exercise, HttpStatus.OK);
+
+        if (exercise == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(exercise);
     }
 }
