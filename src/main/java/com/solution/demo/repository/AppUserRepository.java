07ad6e1c690cbc3279fdd6d411d750d13f0c9fc6
MoseT1 <kimmose981214@gmail.com>
2025-08-14 18:06:47 +0900

    add : findByEmail() 추가

--- src/main/java/com/solution/demo/repository/AppUserRepository.java
+++ src/main/java/com/solution/demo/repository/AppUserRepository.java
@@ -2,10 +2,9 @@ package com.solution.demo.repository;
 
 import com.solution.demo.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    AppUser findByEmail(String email);
-    Optional<AppUser> findByName(String username);
+    // 이메일로 사용자를 조회하고, 결과가 없을 수도 있음을 Optional로 명시
+    Optional<AppUser> findByEmail(String email);
 }
