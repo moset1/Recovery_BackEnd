2a486e54488cfb49a8c03ff73632717b497886f4
MoseT1 <kimmose981214@gmail.com>
2025-08-14 18:09:16 +0900

    fix : @Transactional 도입

--- src/main/java/com/solution/demo/service/ExerciseService.java
+++ src/main/java/com/solution/demo/service/ExerciseService.java
@@ -1,17 +1,20 @@
 package com.solution.demo.service;
 
 import com.solution.demo.entity.Exercise;
+import com.solution.demo.exception.ResourceNotFoundException;
 import com.solution.demo.repository.ExerciseRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class ExerciseService {
     private final ExerciseRepository exerciseRepository;
 
+    @Transactional(readOnly = true)
     public Exercise findByExerciseName(String exerciseName) {
-        return exerciseRepository.findByExerciseName(exerciseName);
+        return exerciseRepository.findByExerciseName(exerciseName)
+                .orElseThrow(() -> new ResourceNotFoundException("Exercise not found with name: " + exerciseName));
     }
 }
