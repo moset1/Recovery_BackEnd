9aedde6050e87315ce9cb4c4512add9f18426dc5
MoseT1 <kimmose981214@gmail.com>
2025-08-18 17:35:34 +0900

    add : 비밀번호 조건 추가

--- src/main/java/com/solution/demo/user/UserCreateForm.java
+++ src/main/java/com/solution/demo/user/UserCreateForm.java
@@ -14,10 +14,11 @@ public class UserCreateForm {
     private String name;
 
     @NotEmpty(message = "비밀번호는 필수항목입니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
     private String password;
 
 
     @NotEmpty(message = "이메일은 필수항목입니다.")
-    @Email
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 }
