6f50d375ed67178cd2d8d1cf4079f261a0df309e
MoseT1 <kimmose981214@gmail.com>
2025-08-18 17:43:34 +0900

    fix : 중복확인 로직 추가

--- src/main/java/com/solution/demo/service/AppUserService.java
+++ src/main/java/com/solution/demo/service/AppUserService.java
@@ -4,6 +4,7 @@ import com.solution.demo.entity.AppUser;
 import com.solution.demo.exception.ResourceNotFoundException;
 import com.solution.demo.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,12 +16,15 @@ public class AppUserService {
     private final AppUserRepository appUserRepository;
     private final PasswordEncoder passwordEncoder;
     @Transactional
-    public void create(String name, String email, String password) {
+    public AppUser create(String name, String email, String password) {
+        if (appUserRepository.findByEmail(email).isPresent()) {
+            throw new DataIntegrityViolationException("이미 등록된 이메일입니다.");
+        }
         AppUser user = new AppUser();
         user.setName(name);
         user.setEmail(email);
         user.setPassword(passwordEncoder.encode(password));
-        this.appUserRepository.save(user);
+        return this.appUserRepository.save(user);
     }
     @Transactional(readOnly = true)
     public AppUser findByEmail(String email) {
